29c491bd4d5e1fbc9fe22414481748defe401288
Radu Matei <radu.matei@fermyon.com>
2022-02-12 09:49:02 +0200

    feat: initial commit for Spin fileserver
    This is the first commit for the Spin HTTP fileserver, a Spin HTTP
    component that can be used to serve static files in Spin applications.
    
    It is designed to be a drop-in replacement for fileserver written
    in Grain and used to currently serve fermyon.com with Wagi.
    As such, it supports setting the desired cache header (currently only
    a global cache, and not per file type, which can be added in the future),
    as well as prefixing the path for resolving files locally.
    
    Signed-off-by: Radu Matei <radu.matei@fermyon.com>

---
+++ readme.md
@@ -0,0 +1,69 @@
+# Static file server for Spin applications
+
+A simple static file server as a [Spin](https://github.com/fermyon/spin) HTTP
+component, written in Rust.
+
+### Using the component as part of a Spin application
+
+Let's have a look at the component definition (from `spin.toml`):
+
+```toml
+[[component]]
+source = "spin_static_fs.wasm"
+id = "fileserver"
+files = ["**/*"]
+[component.trigger]
+route = "/..."
+executor = "spin"
+```
+
+This component will recursively mount all files from the current directory and
+will serve them.
+
+Running the static server:
+
+```shell
+$ spin up --listen 127.0.0.1:3000 --app spin.toml
+```
+
+At this point, the component is going to serve all files in the current
+directory, and this can be tested using `curl`:
+
+```
+$ curl localhost:3000/LICENSE
+                              Apache License
+                        Version 2.0, January 2004
+                    http://www.apache.org/licenses/
+
+TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+...
+```
+
+### Prefixing a path
+
+`PATH_PREFIX` is an environment variable that sets a specific path prefix for
+files before resolving the path on the filesystem, so instead of doing
+`http://example.com/static/static/foo.png`, the environment variable will take
+the `http://example.com/static/foo.png` request and will resolve on the
+filesystem to `static/foo.png` instead of `foo.png`.
+
+### Setting the cache header
+
+Currently, this file server has a single cache header that it can set through
+the `CACHE_CONTROL` environment variable. If no value is set, the default
+`max-age=31536000` is used instead for all media types.
+
+### Building from source and using
+
+Prerequisites:
+
+- [Rust](https://www.rust-lang.org/) at
+  [1.56+](https://www.rust-lang.org/tools/install) with the `wasm32-wasi` target
+  configured
+- [Spin v0.1](https://github.com/fermyon/spin)
+
+Compiling the component:
+
+```shell
+$ cargo build --release
+```
---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+use anyhow::Result;
+use std::{fs::File, io::Read};
+
+// Import the Spin HTTP objects from the generated bindings.
+use spin_http::{Request, Response};
+
+// Generate Rust bindings for interface defined in spin-http.wit file
+wit_bindgen_rust::export!("spin-http.wit");
+
+/// The default value for the cache control header.
+const CACHE_CONTROL_DEFAULT_VALUE: &str = "max-age=31536000";
+/// Environment variable for the cache configuration.
+const CACHE_CONTROL_ENV: &str = "CACHE_CONTROL";
+/// Path prefix.
+const PATH_PREFIX_ENV: &str = "PATH_PREFIX";
+
+/// The Spin HTTP component.
+struct SpinHttp;
+
+impl spin_http::SpinHttp for SpinHttp {
+    /// Implement the `handler` entrypoint for Spin HTTP components.
+    fn handler(req: Request) -> Response {
+        let headers = Some(Self::headers(&req.uri));
+        let (body, status) = match Self::read(&req.uri) {
+            Ok(b) => (Some(b), 200),
+            Err(e) => {
+                eprintln!("Cannot read file: {:?}", e);
+                (Some("404 Not Found.".as_bytes().to_vec()), 404)
+            }
+        };
+
+        Response {
+            status,
+            headers,
+            body,
+        }
+    }
+}
+
+impl SpinHttp {
+    /// Open the file given its path and return its content and content type header.
+    fn read(path: &str) -> Result<Vec<u8>> {
+        let path = match std::env::var(PATH_PREFIX_ENV) {
+            Ok(prefix) => format!("{}{}", prefix, &path[1..path.len()]),
+            Err(_) => path.to_string(),
+        };
+
+        let mut file = File::open(path)?;
+        let mut buf = vec![];
+        file.read_to_end(&mut buf)?;
+
+        Ok(buf)
+    }
+
+    /// Return the media type of the file based on the path.
+    fn mime(uri: &str) -> Option<String> {
+        let guess = mime_guess::from_path(uri);
+        guess.first().map(|m| m.to_string())
+    }
+
+    /// The response headers.
+    fn headers(uri: &str) -> Vec<(String, String)> {
+        let mut headers = vec![];
+        let cache_control = match std::env::var(CACHE_CONTROL_ENV) {
+            Ok(c) => c,
+            Err(_) => CACHE_CONTROL_DEFAULT_VALUE.to_string(),
+        };
+        headers.push((http::header::CACHE_CONTROL.to_string(), cache_control));
+
+        if let Some(m) = Self::mime(uri) {
+            headers.push((http::header::CONTENT_TYPE.to_string(), m));
+        };
+
+        headers
+    }
+}
